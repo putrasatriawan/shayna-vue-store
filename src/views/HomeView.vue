95b9959bc746a03c957732c90a9024ec43bff1dc
Tri Putra Satriawan <tripsatriawan30@gmail.com>
2022-08-08 17:23:18 +0700

    Upload Main Project

---
+++ src/views/HomeView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <HeaderShayna />
+    <HeroShayna />
+    <WomenBannerShayna />
+    <InstaShayna />
+    <PathnerShayna />
+    <FooterShayna />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderShayna from "@/components/HeaderShayna.vue";
+import HeroShayna from "@/components/HeroShayna.vue";
+import WomenBannerShayna from "@/components/WomenBannerShayna.vue";
+import InstaShayna from "@/components/InstaShayna.vue";
+import PathnerShayna from "@/components/PathnerShayna.vue";
+import FooterShayna from "@/components/FooterShayna.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    HeaderShayna,
+    HeroShayna,
+    WomenBannerShayna,
+    InstaShayna,
+    PathnerShayna,
+    FooterShayna,
+  },
+};
+</script>
